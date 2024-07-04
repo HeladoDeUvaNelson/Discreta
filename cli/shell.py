a7bb8091af4e4d0907ba49de7f5daa47aacd6a53
HeladoDeUvaNelson <nelson227@hotmail.es>
2024-07-04 18:51:58 -0300

    [dev] (shell.py) arrow keys detection in command line

--- cli/shell.py
+++ cli/shell.py
@@ -28,11 +28,33 @@ def shell(stdscr):
     pad.addstr(1, 1, ">> ")
     pad.refresh(0, 0, root_win_y-3, 0, root_win_y, root_win_x)
     test = [0, pad.getmaxyx()[1]]
+    i = 0
+    last = []
     while True:
         key = pad.getch(1, pad.getyx()[1])
-        if key == 10: continue
-        if key == '\n': continue
 
+        if key == 10: continue ## enter
+        if key == '\n': continue ## breakline?
+        # if key == 113: break ## q
+        
+        i += 1  
+
+        ## arrow keys
+        if key == 27:
+            key = pad.getch(1, pad.getyx()[1])
+            if key == 91:
+                key = pad.getch(1, pad.getyx()[1])
+                if test[0] > 0:
+                    if key == 68 and curses.getsyx()[1] > 4: 
+                        curses.setsyx(root_win_y-2, curses.getsyx()[1]-1 if test[0] > 0 else 0)
+                        curses.doupdate()
+                    if key == 67 and curses.getsyx()[1]-4 < test[0]: 
+                        curses.setsyx(root_win_y-2, curses.getsyx()[1]+1)
+                        curses.doupdate()
+                continue
+
+        
+        ## return
         if key == 127:
             if test[0] > 0: test[0] -= 1
             string = string[:-pad.getyx()[0]]
@@ -56,30 +78,4 @@ def shell(stdscr):
         pad.addstr(1, 1, ">> " + string)
         pad.refresh(0, 0, root_win_y-3, 0, root_win_y, root_win_x)
 
-    raise Exception("string")
-
-    # command_line_win = curses.newwin(0, 0, root_win_y-3, 0)
-
-    # command_line_text_win = curses.newwin(1, root_win_x-6, root_win_y-2, 4)
-    # # curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    # # GREEN_AND_BLACK = curses.color_pair(1)
-    # command_line_text_win.deleteln()
-    # command_line_text_win.refresh()
-
-    # command_line_win.border()
-    # command_line_win_text_box = Textbox(command_line_text_win)
-
-    # command_line_win.addstr(1, 1, ">> ")
-    # command_line_win.refresh()
-
-    # command_line_win_text_box.edit(enter_is_terminate)
-    
-    
-
-    
-
-# def shell():
-#     global current_command
-#     while current_command != "exit":
-#         queue.append(input(">> "))
-#         current_command = command_map[queue.pop()]()
+    raise Exception(i)
